ea992c7532396380d45804836052673022e40feb
Alejandro Estrada <alejoestradam@yahoo.com>
2023-04-06 12:04:21 -0500

    Tercer modulo

---
+++ MODULO-3/create-library-table.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS library_aem;
+
+CREATE DATABASE IF NOT EXISTS library_aem;
+
+USE library_aem;
+
+CREATE TABLE IF NOT EXISTS authors (
+    author_id INT,
+    nombre  VARCHAR(25) NOT NULL,
+    apellido  VARCHAR(25),
+    genero CHAR(1),
+    fecha_nacimiento  DATE,
+    pais_origen  VARCHAR(40)
+);
+
+INSERT INTO authors (
+    author_id, 
+    nombre, 
+    apellido, 
+    genero, 
+    fecha_nacimiento,
+    pais_origen
+)
+VALUES (3, 'Alejandro', 'Estrada', 'M', '1997-09-30', 'Colombia'),
+       (4, 'Carolina', 'Ramirez', 'F', '1997-09-30', 'Colombia'),
+       (5, 'Isabel', 'Jurado', 'F', '1997-09-30', 'Colombia'),
+       (6, 'Meliza', 'Sanchez', 'F', '1997-09-30', 'Colombia'),
+       (7, '', '', 'F', '1997-09-30', 'Colombia'),
+       (8, 'Milena', 'Cruz', 'F', '1997-09-30', 'Colombia');
+
+SELECT * FROM authors;
